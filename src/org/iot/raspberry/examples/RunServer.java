ea39ae1af8fec112c956c810b031030de294442c
Andre <andreherrera97@hotmail.com>
2018-11-23 11:23:24 -0500

    Deleting comments

--- src/org/iot/raspberry/examples/RunServer.java
+++ src/org/iot/raspberry/examples/RunServer.java
@@ -25,63 +25,16 @@ import rmi.api.iot.SensorRuido;
 public class RunServer {
     public static void main(String[] args) throws Exception {
         GrovePi grovePi = new GrovePi4J();
-        //File control = new File("RUNNINGSAMPLES");
-        //control.deleteOnExit();
-        //Example example = (Example) Class.forName("org.iot.raspberry.examples.encenderledobject" ).newInstance();
-        System.out.println("RUNNING EXAMPLE: " + args[0] + " USING: PI4J");
-        final ExecutorService runner = Executors.newSingleThreadExecutor();
-        //final ExecutorService consoleMonitor = Executors.newSingleThreadExecutor();
-        final ExecutorService fileMonitor = Executors.newSingleThreadExecutor();
-        final Semaphore lock = new Semaphore(0);
-        final Monitor monitor = new Monitor();
         LedObject led = new LedObject(grovePi, 2);
         LCDObject lcd = new LCDObject(grovePi);
         SensorRuidoObject sensorRuido = new SensorRuidoObject(grovePi, 2);
         Registry registry = LocateRegistry.createRegistry(2525);
-
-        runner.execute(() -> {
-            try {
-                System.out.println(args[0]);
-                Led stub = (Led) UnicastRemoteObject.exportObject(led, 0);
-                registry.rebind("Led", stub);
-                //LCD stub2 = (LCD) UnicastRemoteObject.exportObject(lcd,0);
-                //SensorRuido stub3 = (SensorRuido) UnicastRemoteObject.exportObject(sensorRuido,0);
-
-            } catch (Exception ex) {
-                Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            lock.release();
-        });
-
-        //consoleMonitor.execute(() -> {
-        //try (Scanner scanner = new Scanner(System.in)) {
-        // String command;
-        //while (!(command = scanner.next()).equalsIgnoreCase("quit")) {
-        // System.out.println("Command " + command + " not recognized, try quit");
-        // }
-        // }
-        // monitor.stop();
-        // lock.release();
-        // });
-
-        // fileMonitor.execute(() -> {
-        // while (control.exists()) {
-        /// try {
-        // Thread.sleep(100);
-        // } catch (InterruptedException ex) {
-        // }
-        // }
-        //monitor.stop();
-        //lock.release();
-        //});
-
-        lock.acquire();
-        runner.shutdown();
-        //consoleMonitor.shutdownNow();
-        fileMonitor.shutdownNow();
-        runner.awaitTermination(10, TimeUnit.SECONDS);
-        // control.delete();
-        //System.exit(0);
+        System.out.println(args[0]);
+        Led stub = (Led) UnicastRemoteObject.exportObject(led, 0);
+        registry.rebind("Led", stub);
+        //LCD stub2 = (LCD) UnicastRemoteObject.exportObject(lcd,0);
+        //SensorRuido stub3 = (SensorRuido) UnicastRemoteObject.exportObject(sensorRuido,0);
+        System.out.println("RUNNING Server: " + args[0] );
     }
 
 }
