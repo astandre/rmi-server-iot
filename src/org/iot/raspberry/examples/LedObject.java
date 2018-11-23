967130c6abe0e479a80ec9f734c1b71c99396f56
Andre <andreherrera97@hotmail.com>
2018-11-23 11:00:01 -0500

    Formated code

--- src/org/iot/raspberry/examples/LedObject.java
+++ src/org/iot/raspberry/examples/LedObject.java
@@ -1,55 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.iot.raspberry.examples;
+
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.iot.raspberry.grovepi.GroveDigitalOut;
 import org.iot.raspberry.grovepi.GrovePi;
 import rmi.api.iot.Led;
+
 /**
- *
  * @author andre
  */
-public class LedObject implements Led{
-GrovePi grovePi;
-int pin ;
+public class LedObject implements Led {
+    GrovePi grovePi;
+    int pin;
+
     @Override
     public void encenderLed() throws RemoteException {
-        
-    GroveDigitalOut led;
-    try {
-        led = grovePi.getDigitalOut(pin);
-        led.set(true);
-        //Thread.sleep(500);
-        //led.set(false);
-    } catch (IOException ex) {
-        Logger.getLogger(LedObject.class.getName()).log(Level.SEVERE, null, ex);
-    }
+        GroveDigitalOut led;
+        System.out.println("Encendiendo Led");
+        try {
+            led = grovePi.getDigitalOut(pin);
+            led.set(true);
+        } catch (IOException ex) {
+            Logger.getLogger(LedObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    
+
     @Override
     public void apagarLed() throws RemoteException {
-         GroveDigitalOut led;
-    try {
-        led = grovePi.getDigitalOut(pin);
-        led.set(false);
+        GroveDigitalOut led;
+        System.out.println("Apagando Led");
+        try {
+            led = grovePi.getDigitalOut(pin);
+            led.set(false);
 
-    } catch (IOException ex) {
-        Logger.getLogger(LedObject.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LedObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    }
-    public LedObject(GrovePi grovePi,int pin) {
+
+    public LedObject(GrovePi grovePi, int pin) {
         this.grovePi = grovePi;
         this.pin = pin;
     }
-
-
-    
-    
 }
--- src/org/iot/raspberry/examples/RunServer.java
+++ src/org/iot/raspberry/examples/RunServer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.iot.raspberry.examples;
 
 import java.io.File;
@@ -17,6 +12,7 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.iot.raspberry.grovepi.GrovePi;
 import org.iot.raspberry.grovepi.pi4j.GrovePi4J;
 import rmi.api.iot.LCD;
@@ -24,69 +20,68 @@ import rmi.api.iot.Led;
 import rmi.api.iot.SensorRuido;
 
 /**
- *
  * @author andre
  */
 public class RunServer {
-    public static void main(String[] args) throws Exception{
-    GrovePi grovePi = new GrovePi4J();
-     File control = new File("RUNNINGSAMPLES");
-    control.deleteOnExit();
-    //Example example = (Example) Class.forName("org.iot.raspberry.examples.encenderledobject" ).newInstance();
-    System.out.println("RUNNING EXAMPLE: " + args[0] + " USING: PI4J");
-    final ExecutorService runner = Executors.newSingleThreadExecutor();
-    final ExecutorService consoleMonitor = Executors.newSingleThreadExecutor();
-    final ExecutorService fileMonitor = Executors.newSingleThreadExecutor();
-    final Semaphore lock = new Semaphore(0);
-    final Monitor monitor = new Monitor();
-    LedObject led = new LedObject(grovePi,2);
-    LCDObject lcd = new LCDObject(grovePi);
-    SensorRuidoObject sensorRuido = new SensorRuidoObject(grovePi,2);
-    Registry registry =  LocateRegistry.createRegistry(2525);
-   
-    runner.execute(() -> {
-      try {
-          System.out.println(args[0]);
-        Led stub = (Led) UnicastRemoteObject.exportObject(led,0);
-        registry.rebind("Led", stub);
-       //LCD stub2 = (LCD) UnicastRemoteObject.exportObject(lcd,0);
-       //SensorRuido stub3 = (SensorRuido) UnicastRemoteObject.exportObject(sensorRuido,0);
-        
-      } catch (Exception ex) {
-        Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      lock.release();
-    });
+    public static void main(String[] args) throws Exception {
+        GrovePi grovePi = new GrovePi4J();
+        //File control = new File("RUNNINGSAMPLES");
+        //control.deleteOnExit();
+        //Example example = (Example) Class.forName("org.iot.raspberry.examples.encenderledobject" ).newInstance();
+        System.out.println("RUNNING EXAMPLE: " + args[0] + " USING: PI4J");
+        final ExecutorService runner = Executors.newSingleThreadExecutor();
+        //final ExecutorService consoleMonitor = Executors.newSingleThreadExecutor();
+        final ExecutorService fileMonitor = Executors.newSingleThreadExecutor();
+        final Semaphore lock = new Semaphore(0);
+        final Monitor monitor = new Monitor();
+        LedObject led = new LedObject(grovePi, 2);
+        LCDObject lcd = new LCDObject(grovePi);
+        SensorRuidoObject sensorRuido = new SensorRuidoObject(grovePi, 2);
+        Registry registry = LocateRegistry.createRegistry(2525);
 
-    consoleMonitor.execute(() -> {
-      try (Scanner scanner = new Scanner(System.in)) {
-        String command;
-        while (!(command = scanner.next()).equalsIgnoreCase("quit")) {
-          System.out.println("Command " + command + " not recognized, try quit");
-        }
-      }
-      monitor.stop();
-      lock.release();
-    });
+        runner.execute(() -> {
+            try {
+                System.out.println(args[0]);
+                Led stub = (Led) UnicastRemoteObject.exportObject(led, 0);
+                registry.rebind("Led", stub);
+                //LCD stub2 = (LCD) UnicastRemoteObject.exportObject(lcd,0);
+                //SensorRuido stub3 = (SensorRuido) UnicastRemoteObject.exportObject(sensorRuido,0);
 
-    fileMonitor.execute(() -> {
-      while (control.exists()) {
-        try {
-          Thread.sleep(100);
-        } catch (InterruptedException ex) {
-        }
-      }
-      monitor.stop();
-      lock.release();
-    });
+            } catch (Exception ex) {
+                Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            lock.release();
+        });
 
-    lock.acquire();
-    runner.shutdown();
-    consoleMonitor.shutdownNow();
-    fileMonitor.shutdownNow();
-    runner.awaitTermination(10, TimeUnit.SECONDS);
-    control.delete();
-    //System.exit(0);
+        //consoleMonitor.execute(() -> {
+        //try (Scanner scanner = new Scanner(System.in)) {
+        // String command;
+        //while (!(command = scanner.next()).equalsIgnoreCase("quit")) {
+        // System.out.println("Command " + command + " not recognized, try quit");
+        // }
+        // }
+        // monitor.stop();
+        // lock.release();
+        // });
+
+        // fileMonitor.execute(() -> {
+        // while (control.exists()) {
+        /// try {
+        // Thread.sleep(100);
+        // } catch (InterruptedException ex) {
+        // }
+        // }
+        //monitor.stop();
+        //lock.release();
+        //});
+
+        lock.acquire();
+        runner.shutdown();
+        //consoleMonitor.shutdownNow();
+        fileMonitor.shutdownNow();
+        runner.awaitTermination(10, TimeUnit.SECONDS);
+        // control.delete();
+        //System.exit(0);
     }
-    
+
 }
